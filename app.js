3e5830742f4cb9dd2e57c714abc666ec0bd88038
hemantp88 <panchalhemant030700@gmail.com>
2024-06-22 13:30:05 +0530

    setting up view engine ejs

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-    res.send("Hello form yelp");
+    // res.send("Hello form yelp");
+    res.render('home')
 })
+
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
