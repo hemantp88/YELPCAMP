d54f98e134ce878aa5a748f0786fb03c9c0f2b49
hemantp88 <panchalhemant030700@gmail.com>
2024-06-22 13:51:19 +0530

    connecting db and setup testing

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express")
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection Error"));
+db.once("open", () => {
+    console.log("Database Connected");
+})
+
+const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -10,6 +20,11 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'cheam camping' });
+    await camp.save();
+    res.send(camp);
+})
 app.listen(3000, () => {
     console.log("listening on port 3000");
 })
