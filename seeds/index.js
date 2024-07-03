1939e3993ef5fadd37097f3c042386b6da7be077
hemantp88 <panchalhemant030700@gmail.com>
2024-07-03 18:18:37 +0530

    chaging scheman and adding images

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,10 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state} `,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://plus.unsplash.com/premium_photo-1683917067889-c88599491d5c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            description: "loreum"
+
         })
         await camp.save()
     }
