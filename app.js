35b84a88e3d51d1190fefa1e0fca1b01fdb467a0
hemantp88 <panchalhemant030700@gmail.com>
2024-07-08 17:02:55 +0530

    adding joi scheman validation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 const ExpressError = require("./utils/ExpressError");
 const ejsMate = require('ejs-mate');
-
+const Joi = require('joi')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 const db = mongoose.connection;
@@ -55,7 +55,23 @@ app.get('/campgrounds/new', (req, res) => {
 })
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
     // res.send(req.body.campground);
-    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string.required()
+
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+    // console.log(result);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
     const { title, location, image, description, price } = req.body.campground;
     const campground = new Campground({ location: location, title: title, image: image, description: description, price: price });
     await campground.save();
