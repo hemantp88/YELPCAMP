082534f0cf68fb6938271beb4dce2bde74dd6fd9
hemantp88 <panchalhemant030700@gmail.com>
2024-07-13 18:19:34 +0530

    deleting an review from a campground

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }))
 app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground)
+    // console.log(campground)
     res.render('campgrounds/show', { campground });
 }))
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
@@ -127,6 +127,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    // res.send("deleting me!!!");
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 app.all('*', (req, res, next) => {
     // res.send("404!!!!");
     next(new ExpressError("Pgae Not found", 404));
