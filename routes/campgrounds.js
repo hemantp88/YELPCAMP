2f9a64f0d643c2bfabfef75e3faac5df63879567
hemantp88 <panchalhemant030700@gmail.com>
2024-07-15 15:20:52 +0530

    flash success partials by setting up flash for creation deletion and updation fo campground and review too

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -47,12 +47,13 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
-
+    req.flash('success', 'Successfully updated a campground');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted a campground');
     res.redirect('/campgrounds');
 
 }))
