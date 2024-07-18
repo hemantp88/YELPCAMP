15bc8b1b1cc08a18386c70123d40bb0560bab9bf
hemantp88 <panchalhemant030700@gmail.com>
2024-07-18 17:03:53 +0530

    using isloogied in middleware on reviews too and showing review div when logged in

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,12 +5,13 @@ const Campground = require('../models/campground');
 const ExpressError = require("../utils/ExpressError");
 
 const Review = require('../models/review.js')
-const { validateReview } = require('../middleware.js')
+const { validateReview, isLoggedIn } = require('../middleware.js')
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
