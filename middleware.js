f7ca1017d2804a1452ba70800e90e5edae54ec87
hemantp88 <panchalhemant030700@gmail.com>
2024-07-18 16:51:22 +0530

    authorization middleware

--- middleware.js
+++ middleware.js
@@ -1,7 +1,9 @@
-//  if (!req.isAuthenticated()) {
-//         req.flash('error', 'You must be signed in!!');
-//         return res.redirect('/login')
-//     }
+const Campground = require('./models/campground');
+const { campgroundSchema } = require('./schemas');
+const ExpressError = require("./utils/ExpressError");
+const { reviewSchema } = require('./schemas');
+
+
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
@@ -16,4 +18,38 @@ module.exports.isLoggedIn = (req, res, next) => {
         return res.redirect('/login');
     }
     next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+}
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that');
+        res.redirect(`/campgrounds/${id}`);
+    } next();
+
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    // console.log(error);
+    if (error) {
+        // console.log(error)
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,21 +3,9 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const Campground = require('../models/campground');
 const ExpressError = require("../utils/ExpressError");
-const { reviewSchema } = require('../schemas.js');
-const Review = require('../models/review.js')
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    // console.log(error);
-    if (error) {
-        // console.log(error)
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    else {
-        next();
-    }
-}
+const Review = require('../models/review.js')
+const { validateReview } = require('../middleware.js')
 
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
@@ -35,7 +23,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    req.flash('success ', 'Successfully deleted a review');
+    req.flash('success', 'Successfully deleted a review');
     res.redirect(`/campgrounds/${id}`);
 }))
 
